3aa13a57cd9f2e01eac49b47e18eca5d90ffe78f
YHY <snxamdf@126.com>
2016-05-21 10:20:31 +0800

    u

---
+++ web-medicine/src/main/java/com/gdm/medicine/App1Application.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ *
+ * 
+ */
+package com.gdm.medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.gdm" })
+public class App1Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App1Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App1Application.class, args);
+	}
+
+}
---
+++ web-medicine/src/main/java/com/gdm/medicine/domain/CmsAffiche.java
@@ -0,0 +1,54 @@
+/*
+ * 
+ *
+ * 
+ */
+package com.gdm.medicine.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.gdm.core.domain.Sys;
+
+/**
+ * 公告表Domain
+ * 
+ * @author yanghongyan
+ * @version 2015-01-12
+ * @----------------------------------------------------------------------------------------
+ * @updated 修改描述.
+ * @updated by yanghongyan
+ * @updated at 2015-01-12
+ */
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "t_cms_affiche")
+public class CmsAffiche extends Sys<String> {
+	private static final long serialVersionUID = -4612145003980136767L;
+	public static final String STATUS_YES = "1";//显示
+	public static final String STATUS_NO = "0";//不显示
+	public static final String STATE_YES = "1";//置顶
+	public static final String STATE_NO = "0";//不置顶
+	public static final String CT="0";//用来判断页面展示 内容判断
+	public static final String TYPE="affiche";//用来判断是哪个详细页面
+
+	@NotBlank(message = "标题不能为空")
+	@Length(max = 60, message = "字数太多了，只能输入最多60个字符")
+	private String title;
+	@NotBlank(message = "正文不能为空")
+	private String content;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date publishTime;
+	private String state;
+	private String status;
+
+}
